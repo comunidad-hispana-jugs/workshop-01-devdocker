289f394c7d9c5aedae5d1fec5d3bdb5f74512bac
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2018-02-24 11:35:04 +0100

    Revert "update config to replace Postgre with Derby in memory"
    This reverts commit 44cb92161ac2bece30b937f69a1517629e301e33.

--- src/main/resources/db/migration/V1__Create_Reservations_Table.sql
+++ src/main/resources/db/migration/V1__Create_Reservations_Table.sql
@@ -1,20 +1,23 @@
 -- This DDL is used by flyway to initially setup the table
 -- which will hold the reservation entries
+--
+
+
+CREATE SEQUENCE reservation_id_seq;
 
 -- Simple table for holding reservations
-create table reservation (
+create table public.reservation (
  -- id for this table
-    id integer not null generated always as identity (start with 1, increment BY 1),
+ 	id integer NOT NULL DEFAULT nextval('reservation_id_seq'),
  -- reservation owner
-    reservation_owner varchar(300),
+    reservation_owner varchar(300) null,
  -- restaurant name
-    restaurant_name varchar(300),
+    restaurant_name varchar(300) null,
  -- reservation date
-    reservation_date date,
+    reservation_date date null,
  -- diners number
-    diners_number varchar(5),
+    diners_number varchar(5) null,
  -- confirmed reservation
-    confirmed boolean,
- -- the primary key
-    constraint primary_key primary key (id)
+    confirmed boolean
 );
+ALTER SEQUENCE reservation_id_seq OWNED BY Reservation.id;
